da77daa0ccd6c72e9b1a9a82c9e183a6865f6764
macnoblebk <macnoblebk@gmail.com>
2023-08-31 15:05:19 -0700

    Initial commit

---
+++ assignment2/include/ScopedArray.h
@@ -0,0 +1,10 @@
+// Author: Mac-Noble Brako-Kusi
+// File: ScopedArray.h
+// Date: August 31, 2023
+// Purpose: Declaration file for ScopedArray class
+//
+
+#ifndef ASSIGNMENT1_SOLUTION_SCOPEDARRAY_H
+#define ASSIGNMENT1_SOLUTION_SCOPEDARRAY_H
+
+#endif //ASSIGNMENT1_SOLUTION_SCOPEDARRAY_H
---
+++ assignment2/src/ScopedArray.cpp
@@ -0,0 +1,5 @@
+// Author: Mac-Noble Brako-Kusi
+// File: ScopedArray.h
+// Date: August 31, 2023
+// Purpose: Implementation file for ScopedArray class
+//
