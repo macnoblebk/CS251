d43496c471a8ba2ade132667fcecc66b43825ee8
macnoblebk <macnoblebk@gmail.com>
2023-08-31 22:41:35 -0700

    Declares interface of ScopedArray class

--- assignment2/include/ScopedArray.h
+++ assignment2/include/ScopedArray.h
@@ -2,9 +2,42 @@
 // File: ScopedArray.h
 // Date: August 31, 2023
 // Purpose: Declaration file for ScopedArray class
-//
 
-#ifndef ASSIGNMENT1_SOLUTION_SCOPEDARRAY_H
-#define ASSIGNMENT1_SOLUTION_SCOPEDARRAY_H
 
-#endif //ASSIGNMENT1_SOLUTION_SCOPEDARRAY_H
+#ifndef MY_SCOPED_ARRAY_H
+#define MY_SCOPED_ARRAY_H
+
+#include "iostream"
+
+template<typename T>
+class ScopedArray {
+
+    public:
+         explicit ScopedArray(T* ptr = nullptr);
+
+        ~ScopedArray();
+
+         T* get() const;
+
+         const T& operator[](size_t) const;
+
+         T& operator[](size_t) ;
+
+         explicit operator bool() const;
+
+         T* release();
+
+         void reset(T* rhs = nullptr);
+
+         void swap(ScopedArray& rhs);
+
+    private:
+        ScopedArray(const ScopedArray&) = delete;
+        const ScopedArray& operator=(const ScopedArray&) = delete;
+
+        T* scopedArray_;
+};
+
+
+#include "../src/ScopedArray.cpp"
+#endif // MY_SCOPED_ARRAY_H
--- assignment2/src/ScopedArray.cpp
+++ assignment2/src/ScopedArray.cpp
@@ -1,5 +1,3 @@
-// Author: Mac-Noble Brako-Kusi
-// File: ScopedArray.h
-// Date: August 31, 2023
-// Purpose: Implementation file for ScopedArray class
+//
+// Created by Mac-Noble Brako-Kusi on 8/31/23.
 //
