7baede57f6e8e5bbb039cd6f365a7fa3c0ad1b7a
macnoblebk <macnoblebk@gmail.com>
2023-09-01 12:51:26 -0700

    Documents header file of ScopedArray class

--- assignment2/include/ScopedArray.h
+++ assignment2/include/ScopedArray.h
@@ -13,28 +13,41 @@ template<typename T>
 class ScopedArray {
 
     public:
+         // Constructor: Creates a ScopedArray object with an optional pointer to dynamically allocated memory.
          explicit ScopedArray(T* ptr = nullptr);
 
+        // Destructor: Deallocates the memory held by the ScopedArray object.
         ~ScopedArray();
 
+         // Returns a pointer to the held memory.
          T* get() const;
 
+         // Accessor operator for reading values from the held array.
          const T& operator[](size_t) const;
 
+         // Accessor operator for modifying values in the held array.
          T& operator[](size_t) ;
 
+         // Explicit conversion operator to check if the ScopedArray holds a valid pointer.
          explicit operator bool() const;
 
+         // Releases ownership of the held memory and returns the pointer without deallocating it.
          T* release();
 
+         // Resets the ScopedArray to point to a new memory location, optionally deallocating the previous memory.
          void reset(T* rhs = nullptr);
 
+         // Swaps the contents of two ScopedArray objects.
          void swap(ScopedArray& rhs);
 
     private:
+        // Disallow copy construction.
         ScopedArray(const ScopedArray&) = delete;
+
+        // Disallow copy assignment.
         const ScopedArray& operator=(const ScopedArray&) = delete;
 
+        // Pointer to the dynamically allocated memory.
         T* scopedArray_;
 };
 
