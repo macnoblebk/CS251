490e1b2b35e0d0f4aaa40d2003712f58f83c0ce9
macnoblebk <macnoblebk@gmail.com>
2023-09-14 23:52:59 -0700

    Implements ArrayList copy constructor

--- assignment3/src/ArrayList.cpp
+++ assignment3/src/ArrayList.cpp
@@ -9,10 +9,20 @@ template<typename T>
 ArrayList<T>::ArrayList()
     : mSize(0),
       mCapacity(0),
-      mArray(nullptr){}
+      mArray(nullptr) {}
 
 template<typename T>
 ArrayList<T>::ArrayList(const uint32_t &size, const T &value)
         : mSize(size),
           mCapacity(2*size),
-          mArray{value}{}
+          mArray{value} {}
+
+template<typename T>
+ArrayList<T>::ArrayList(const ArrayList<T> &src)
+    : mSize(src.mSize),
+      mCapacity(src.mCapacity),
+      mArray(std::make_unique<ScopedArray<T[]>>(src.mCapacity)) {
+          for (size_t i = 0; i < src.mSize; ++i)
+              mArray[i] = src.mArray[i];
+      }
+
